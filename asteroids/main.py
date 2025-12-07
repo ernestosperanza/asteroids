1bad8a1749f09d2dc6c3e8b0da80caf13c490d6b
ernestosperanza <29267672+ernestosperanza@users.noreply.github.com>
2025-12-07 17:59:58 -0300

    feat: spaceship logic

--- asteroids/main.py
+++ asteroids/main.py
@@ -1,23 +1,40 @@
 import pygame
+from player import Player
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
     pygame.init()
+    # define groups and assing
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     version = pygame.version.ver
     print(f"Starting Asteroids with pygame version: {version}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     while(True):
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
-        pygame.display.flip()
 
+        updatable.update(dt)
+        screen.fill("black")
+        
+        for ele in drawable:
+            ele.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
